c50cce626099d39566d2c8f7686a2060c65b20cc
minjun <qkralswns1998@gmail.com>
2024-11-13 17:56:58 +0900

    문제 해결.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import React, { useState, useEffect } from "react";
 import TextInput from "./components/TextInput";
 import TextList from "./components/TextList";
+import TodoListContext from "./context/context";
 
 function App() {
-  // TODO: texts 를 context api 로 리팩터링 하세요.
-  const [texts, setTexts] = useState(() =>
-    localStorage.getItem("texts")
-      ? JSON.parse(localStorage.getItem("texts"))
-      : [],
-  );
+    // TODO: texts 를 context api 로 리팩터링 하세요.
+    const [texts, setTexts] = useState(() =>
+        localStorage.getItem("texts")
+            ? JSON.parse(localStorage.getItem("texts"))
+            : []
+    );
 
-  useEffect(() => {
-    localStorage.setItem("texts", JSON.stringify(texts));
-  }, [texts]);
+    useEffect(() => {
+        localStorage.setItem("texts", JSON.stringify(texts));
+    }, [texts]);
 
-  const onAddText = (text) => {
-    setTexts((prevTexts) => [...prevTexts, text]);
-  };
+    const onAddText = (text) => {
+        setTexts((prevTexts) => [...prevTexts, text]);
+    };
 
-  return (
-    <div>
-      <h1>Text Input and Listing</h1>
-      <TextInput onAddText={onAddText} />
-      <TextList texts={texts} />
-    </div>
-  );
+    return (
+        <TodoListContext.Provider value={{ texts }}>
+            <div>
+                <h1>Text Input and Listing</h1>
+                <TextInput onAddText={onAddText} />
+                <TextList />
+            </div>
+        </TodoListContext.Provider>
+    );
 }
 
 export default App;
--- src/components/TextList.jsx
+++ src/components/TextList.jsx
@@ -1,9 +1,14 @@
-export default function TextList({ texts }) {
-  return (
-    <ul>
-      {texts.map((text, index) => (
-        <li key={index}>{text}</li>
-      ))}
-    </ul>
-  );
+import { useContext } from "react";
+import TodoListContext from "../context/context";
+
+export default function TextList() {
+    const data = useContext(TodoListContext);
+    const { texts } = data;
+    return (
+        <ul>
+            {texts.map((text, index) => (
+                <li key={index}>{text}</li>
+            ))}
+        </ul>
+    );
 }
